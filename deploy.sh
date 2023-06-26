388d78cf28a803e3ad7cc66cc0ebf1d6f23f19e9
adeliberraken <adel.iberraken@stagiairesmns.fr>
2023-06-26 15:23:53 +0200

    changement deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,13 @@ git pull
 bash mvnw package -P prod,sysadmin  --settings /home/debian/.m2/settings.xml
 
 # Construire l'image Docker
-docker build --no-cache -t spring-demo-franck .
+docker build --no-cache -t locmns_back .
 
 # Arreter le conteneur existant
-docker stop spring-demo-franck
+docker stop locmns_back
 
 # Supprimer le conteneur existant
-docker rm spring-demo-franck
+docker rm locmns_back
 
 # Lancer un nouveau conteneur
-docker run -d --net backend --ip 172.18.0.5 --name=spring-demo-franck -p 8181:8080 -v upload_spring_mns_23:/uploads spring-demo-franck
+docker run -d --net backend --ip 172.18.0.5 --name=locmns_back -p 8181:8080 -v uploaded_files:/uploads locmns_back
