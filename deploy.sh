3b561e9bab6d4e1abaf9e367dfce70d20fa75d01
adeliberraken <adel.iberraken@stagiairesmns.fr>
2023-06-26 15:53:22 +0200

    tkt

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ git pull
 bash mvnw package -P prod,sysadmin  --settings /home/debian/.m2/settings.xml
 
 # Construire l'image Docker
-docker build --no-cache -t locmns .
+docker build --no-cache -t locmns-back .
 
 # Arreter le conteneur existant
 docker stop locmns
@@ -16,4 +16,4 @@ docker stop locmns
 docker rm locmns
 
 # Lancer un nouveau conteneur
-docker run -d --net backend --ip 172.18.0.5 --name=locmns -p 8181:8080 -v uploaded_files:/uploads locmns
+docker run -d --net backend --ip 172.18.0.5 --name=locmns -p 8181:8080 -v uploaded_files:/uploads locmns-back
