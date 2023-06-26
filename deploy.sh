a151f63fa5ad907a786c6e2fe3cd94c84ed5a97b
adeliberraken <adel.iberraken@stagiairesmns.fr>
2023-06-26 15:35:28 +0200

    changement nom locmns

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,13 @@ git pull
 bash mvnw package -P prod,sysadmin  --settings /home/debian/.m2/settings.xml
 
 # Construire l'image Docker
-docker build --no-cache -t locmns_back .
+docker build --no-cache -t locmns .
 
 # Arreter le conteneur existant
-docker stop locmns_back
+docker stop locmns
 
 # Supprimer le conteneur existant
-docker rm locmns_back
+docker rm locmns
 
 # Lancer un nouveau conteneur
-docker run -d --net backend --ip 172.18.0.5 --name=locmns_back -p 8181:8080 -v uploaded_files:/uploads locmns_back
+docker run -d --net backend --ip 172.18.0.5 --name=locmns -p 8181:8080 -v uploaded_files:/uploads locmns
